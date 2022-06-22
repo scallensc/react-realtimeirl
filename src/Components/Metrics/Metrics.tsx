a55ea581d15504e902d54a4ae05514a6195bf213
Samuel Allen <samuelcallen@me.com>
2022-06-23 00:24:56 +0800

    remove heading text, replace solely with compass

--- src/Components/Metrics/Metrics.tsx
+++ src/Components/Metrics/Metrics.tsx
@@ -24,12 +24,12 @@ const OtherMetrics = () => {
   }, [])
   const [state] = useContext(stateContext);
   return (
-    <div className="other-container" style={{display:metrics ? '' : 'none'}}>
+    <div className="metrics-container" style={{display:metrics ? '' : 'none'}}>
       <div className="heart-text" style={{display:heartrate ? '' : 'none'}}>{state.heartRate === 0 ? `Heartrate: ${state.heartRate} bpm` : ''}</div>
       <div className="altitude-text" style={{display:altitude ? '' : 'none'}}>Altitude: {state.imperial ? `${(Math.abs((state.altitude['EGM96'] - state.altitude['WGS84']) / 2 ) * 3.281).toFixed(0)} ft` : `${Math.abs((state.altitude['EGM96'] - state.altitude['WGS84']) / 2 ).toFixed(0)} m`}</div>
       <div className="speed-text" style={{display:speed ? '' : 'none', paddingBottom: speedPad ? '8px' : ''}}>Speed: {state.imperial ? `${(state.speed / 1.609).toFixed()} mp/h` : `${(state.speed).toFixed()} km/h`}</div>
       <div className="distance-text" style={{display:distance ? '' : 'none'}}>{state.imperial ? `Total distance: ${(state.totalDistance / 1.609).toFixed(2)} mi` : `Total distance: ${(state.totalDistance).toFixed(2)} km`}</div>
-      <div className="heading-text" style={{display:heading ? '' : 'none'}}>Heading: {state.headingCardinal} - {`${state.headingDegrees.toFixed()}°`}&nbsp;      
+      <div className="heading-text" style={{display:heading ? '' : 'none'}}>Heading:&nbsp;     
         <div className="compass-icon">
           <SvgLoader 
             path={`assets/compass.svg`}
